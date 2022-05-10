7f7c77f5a969c958d0c9dfd7747060b0a4d2fcfb
Murad <mchowdh200@gmail.com>
2022-05-10 11:30:15 -0600

    add install instructions

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/env bash
+mkdir -p ../workflows/bin
 
 git clone https://github.com/ryanlayer/giggle.git
 cd giggle
 make
+mv bin/giggle ../workflows/bin/
 
 cd ..
 wget http://www.sqlite.org/2017/sqlite-amalgamation-3170000.zip
@@ -11,3 +13,5 @@ unzip sqlite-amalgamation-3170000.zip
 git clone https://github.com/ryanlayer/stix.git
 cd stix
 make
+mv bin/stix ../workflows/bin/
+
