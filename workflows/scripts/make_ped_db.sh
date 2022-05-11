6dc2ee04ecaa306ca6b24f0fbf123c87eefdd796
Murad <mchowdh200@gmail.com>
2022-05-11 15:39:23 -0600

    wron var name

--- workflows/scripts/make_ped_db.sh
+++ workflows/scripts/make_ped_db.sh
@@ -6,6 +6,6 @@ outdir=$3
 stix_bin=$4
 
 cd $outdir
-$stix_bin -i $(basename $beds) -p $(basename $ped) -d $(basename $ped).db -c 8
+$stix_bin -i $(basename $giggle_index) -p $(basename $ped) -d $(basename $ped).db -c 8
 # $stix_bin -i $giggle_index -p $ped -d $ped.db -c 8
 
