e451737d792c84ce416bb8bb3f05354ccc3ca1d7
Murad <mchowdh200@gmail.com>
2022-05-11 15:34:10 -0600

    wrong column

--- workflows/scripts/make_ped_db.sh
+++ workflows/scripts/make_ped_db.sh
@@ -7,5 +7,5 @@ stix_bin=$4
 
 # cd $outdir
 # $stix_bin -i $(basename $beds) -p $(basename $ped) -d $(basename $ped).db -c 7
-$stix_bin -i $giggle_index -p $ped -d $ped.db -c 7
+$stix_bin -i $giggle_index -p $ped -d $ped.db -c 8
 
