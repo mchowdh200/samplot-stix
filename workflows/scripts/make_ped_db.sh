8992bc16790bb654689692cb2d63c192fceacd13
Murad <mchowdh200@gmail.com>
2022-05-11 15:35:31 -0600

    now try cd method

--- workflows/scripts/make_ped_db.sh
+++ workflows/scripts/make_ped_db.sh
@@ -5,7 +5,7 @@ ped=$2
 outdir=$3
 stix_bin=$4
 
-# cd $outdir
-# $stix_bin -i $(basename $beds) -p $(basename $ped) -d $(basename $ped).db -c 7
-$stix_bin -i $giggle_index -p $ped -d $ped.db -c 8
+cd $outdir
+$stix_bin -i $(basename $beds) -p $(basename $ped) -d $(basename $ped).db -c 8
+# $stix_bin -i $giggle_index -p $ped -d $ped.db -c 8
 
