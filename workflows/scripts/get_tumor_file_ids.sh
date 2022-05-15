683f3d4b835a356974a078b6382e52e26f79b4ca
Murad <mchowdh200@gmail.com>
2022-05-15 09:50:38 -0600

    be more specific

--- workflows/scripts/get_tumor_file_ids.sh
+++ workflows/scripts/get_tumor_file_ids.sh
@@ -10,8 +10,10 @@ donor_table=$2
 normal_out=$3
 tumor_out=$4
 fid2type=$(tail -n+2 $donor_table | cut -f2,7)
-normal_ids=$(echo "$fid2type" | grep -i normal | cut -f1 | tr '\n' '|')
-tumor_ids=$(echo "$fid2type" | grep -i tumour | cut -f1 | tr '\n' '|')
+normal_ids=$(echo "$fid2type" | grep -i normal | cut -f1 |
+                 awk '{print $0".excord.bed.gz"}' | tr '\n' '|')
+tumor_ids=$(echo "$fid2type" | grep -i tumour | cut -f1 |
+                awk '{print $0".excord.bed.gz"}' | tr '\n' '|')
 
 ls $indir | grep -i -E \"$normal_ids\" > $normal_out
 ls $indir | grep -i -E \"$tumor_ids\" > $tumor_out
